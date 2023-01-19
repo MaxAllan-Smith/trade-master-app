9ac2c8f87122b11efb4f4dd5a33d116f0f9cc109
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-19 20:43:50 +0000

    Initialized Program.rb and wrote first Rspec

---
+++ lib/Program.rb
@@ -0,0 +1,7 @@
+
+
+class Program
+  def Initialize
+    
+  end
+end
--- spec/Program_Spec.rb
+++ spec/Program_Spec.rb
@@ -1,2 +1,10 @@
 require "Program"
 
+describe Program do
+  context "Authentication Error:" do
+    it "Requires an Input For Username." do
+      userLogin = Program.new
+      expect(userLogin.Login(userName)).to raise_error("No Username Was Supplied.")
+    end
+  end
+end
